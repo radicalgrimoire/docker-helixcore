1bb07a21ad90193822995e07cb2dc74a466b4a93
UenoSouhei <radicalgrimoire@gmail.com>
2022-11-29 16:16:15 +0900

    環境変数の追加

--- build/files/init.sh
+++ build/files/init.sh
@@ -6,6 +6,11 @@ if ! p4dctl list 2>/dev/null | grep -q $P4NAME; then
 echo bash /opt/perforce/sbin/configure-helix-p4d.sh $P4NAME -n -p $P4PORT -r $P4ROOT -u $P4USER -P $P4PASSWD --case $CASE_INSENSITIVE --unicode
 p4 trust -y -f
 
+p4 configure set server.extensions.allow.unsigned=1
+p4 configure set net.keepalive.idle=10
+p4 configure set net.keepalive.interval=30
+p4 configure set net.keepalive.count=3
+
 cat > ~perforce/.p4config <<EOF
 P4USER=$P4USER
 P4PORT=$P4PORT
