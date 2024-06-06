6787e773da85fb9b01b7b7ad89b789ff753fa048
ueno.s <ueno.s@gamestudio.co.jp>
2024-06-06 16:11:55 +0900

    trust をするように修正

--- build/files/run.sh
+++ build/files/run.sh
@@ -3,5 +3,6 @@ set -e
 
 p4dctl start -t p4d $P4NAME
 sudo service cron start
+p4 trust -y -f
 yes $P4PASSWD | p4 login
 exec /usr/bin/tail --pid=$(cat /var/run/p4d.$P4NAME.pid) -F "$P4ROOT/logs/log"
